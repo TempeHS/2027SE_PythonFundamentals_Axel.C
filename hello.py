8bf154d6cd310bb0e580f84c6399052fb6161476
Axel C <159206642+TheSpiciestRamen@users.noreply.github.com>
2026-02-10 20:51:33 +0000

    Started hello.py file

---
+++ hello.py
@@ -0,0 +1,17 @@
+# asks for users name
+# name = input("What is your name? ")
+
+# The following functions are calling on built in python methods associated with strings (str)
+
+# remove whitespace from str
+# name = name.strip()
+# capitalize user's name by having the first letter of every word capitalized
+# name = name.title()
+
+# or alternatively:
+
+name = input("What is your name? ").strip().title()
+
+
+# say hello to user
+print("hello, " + name)

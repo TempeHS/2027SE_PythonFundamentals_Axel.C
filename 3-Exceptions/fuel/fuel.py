93c704ae9c1abfa10b30645da4fdc74abca7fc3c
Axel C <159206642+TheSpiciestRamen@users.noreply.github.com>
2026-03-02 22:41:20 +0000

    fixed fuel.py

--- 3-Exceptions/fuel/fuel.py
+++ 3-Exceptions/fuel/fuel.py
@@ -1,16 +1,17 @@
-x, y = map(int, input("Please enter a fraction: ").split("/"))
 fuel_level = 0
 try:
+    x, y = map(int, input("Please enter a fraction: ").split("/"))
     fuel_level = (x / y) * 100
 except ValueError:
     print("Values given were not integers.")
 except ZeroDivisionError:
     print("Fuel level given is impossible.")
-
-
-if fuel_level <= 1:
-    print("E")
-elif fuel_level >= 99:
-    print("F")
 else:
-    print(str(round(fuel_level)) + "%" + " fuel remaining.")
+    if fuel_level > 100:
+        print("Fuel level given is impossible.")
+    elif fuel_level <= 1:
+        print("E")
+    elif fuel_level >= 99:
+        print("F")
+    else:
+        print(str(round(fuel_level)) + "%" + " fuel remaining.")

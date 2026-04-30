e0afb7941ccdc1074ed7fe0dc7f78f2105b50b10
Axel C <159206642+TheSpiciestRamen@users.noreply.github.com>
2026-04-30 00:26:17 +0000

    Upload completed lines.py

--- 6-FileIO/lines/lines.py
+++ 6-FileIO/lines/lines.py
@@ -5,28 +5,28 @@ file_length = 0
 # checks length and stuff
 
 if len(sys.argv) != 2:
-    sys.exit()
+    sys.exit("Incorrect number of command line arguments.")
 
 # idk these comments are here as a test
 
 if sys.argv[1].endswith(".py"):
     pass
 else:
-    sys.exit()
+    sys.exit("Incorrect file type given.")
 
 
 try:
     with open(sys.argv[1], "r") as user_given_file:
         for line in user_given_file:
             line = line.lstrip()
-            if line.isprintable() or line.startswith("#"):
+            if line.isprintable() or line.startswith("#") or line.startswith(" "):
                 pass
             else:
                 file_length += 1
-                print(line.lstrip())
+                print(line.lstrip(), end=" ")  # this is here fpr debugging
                 print("ENDLINE")
 except FileNotFoundError:
-    sys.exit()
+    sys.exit("File given does not exist.")
 
 
 print(f"The chosen file has {file_length} lines.")

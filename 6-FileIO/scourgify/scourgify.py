eed7906a7c65dd1ec1759f92e7f4ec9b17c14eb6
Axel C <159206642+TheSpiciestRamen@users.noreply.github.com>
2026-05-08 02:18:35 +0000

    Upload completed scourgify.py

--- 6-FileIO/scourgify/scourgify.py
+++ 6-FileIO/scourgify/scourgify.py
@@ -8,3 +8,18 @@ if sys.argv[1].endswith(".csv"):
     pass
 else:
     sys.exit("Incorrect file type given.")
+
+
+try:
+    with open(sys.argv[2], "a") as file_write:
+        writer = csv.DictWriter(file_write, fieldnames=["first", "last", "house"])
+        writer.writeheader()
+        with open(sys.argv[1]) as file_read:
+            reader = csv.DictReader(file_read)
+            for row in reader:
+                lastN, firstN = row["name"].split(",")
+                writer.writerow(
+                    {"first": firstN.lstrip(), "last": lastN, "house": row["house"]}
+                )
+except FileNotFoundError:
+    sys.exit("One or more files provided do not exist.")
